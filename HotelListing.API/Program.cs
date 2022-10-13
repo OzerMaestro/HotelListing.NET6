0efeb8ca4c7f88e70a0f97058a0b98200678422c
mehme <83847321+OzerMaestro@users.noreply.github.com>
2022-10-13 23:44:55 +0200

    swagger reload changes

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -105,7 +105,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiDemo v1");
+            c.RoutePrefix = "";
+        });
 }
 
 app.UseMiddleware<ExceptionMiddleware>();
