d6264077ec37e802363157655b9abcc487cedf25
meo <83847321+OzerMaestro@users.noreply.github.com>
2022-10-14 16:08:40 +0200

    refactoring repos

--- HotelListing.API.Core/Interfaces/IGenericRepository.cs
+++ HotelListing.API.Core/Interfaces/IGenericRepository.cs
@@ -4,12 +4,16 @@ namespace HotelListing.API.Contracts
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<T> GetAsync(int? id);
-        Task<List<T>> GetAllAsync();
+        //Task<T> GetAsync(int? id);
+        Task<TResult> GetAsync<TResult>(int? id);
+        //Task<List<T>> GetAllAsync();
+        Task<List<TResult>> GetAllAsync<TResult>();
         Task<PagedResult<TResult>> GetAllAsync<TResult>(QueryParameters queryParameters);
-        Task<T> AddAsync(T entity);
+        //Task<T> AddAsync(T entity);
+        Task<TResult> AddAsync<TSource, TResult>(TSource source);
         Task DeleteAsync(int id);
-        Task UpdateAsync(T entity);
+        //Task UpdateAsync(T entity);
+        Task UpdateAsync<TSource>(int id, TSource source);
         Task<bool> Exists(int id);
     }
 }
